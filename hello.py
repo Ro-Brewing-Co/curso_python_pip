31db654c1cf0d0ff40e2b71ecb26d51f7ca2cfbe
Ro Brewing Co <kladel8@gmail.com>
2023-02-18 11:17:53 -0500

    Segunda vez que guardo

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
 print("Hola desde mi máquina")
+
+print(" Para conectar tu pc con github, debes tener encuanta el proceso de la llave ssh...")
+print("Generar una nueva llave SSH: Cualquier sistema operativo ssh-keygen -t rsa -b 4096 -C "youremail@example.com" ")
+
+print("printtComprobar proceso y agregarlo (Windows) eval $(ssh-agent - s) ssh-add ~/.ssh/id_rsa")
+#Comprobar proceso y agregarlo (Mac)
+
+#eval "$(ssh-agent -s)"
+#¿Usas macOS Sierra 10.12.2 o superior?
+#Haz lo siguiente:
+
+#cd ~/.ssh
+#Crea un archivo config…
+#Con Vim vim config
+#Con VSCode code config
+#Pega la siguiente configuración en el archivo…")
