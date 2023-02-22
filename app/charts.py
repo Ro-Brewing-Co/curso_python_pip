d07ff10176300c74a7831321b389fbe30fa8975f
Ro Brewing Co <kladel8@gmail.com>
2023-02-21 22:46:25 -0500

    Aquí se generan las imágenes por país en una carpeta llamada imgs

--- app/charts.py
+++ app/charts.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt  #Esta es una libreria que no está incorporado 
 
 #se puede poner un alias a matplotlib.pyplot con as plt y así se puede llamar todo con plt
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values) #Esto es para las barras, que lavels sea eje x y values sea en eje y y que salgan barras
-  plt.savefig("bar.png")#Para graficar y se grafica en output, se llama en python
+  plt.savefig(f"./imgs/{name}.png")#Para graficar y se grafica en output, se llama en python
   plt.close()
 
 def generate_pie_chart(labels, values):
--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def run():
     country = result[0]
     print(country)
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(country["Country/Territory"], labels, values)
 
 if __name__ == '__main__':
   run()
